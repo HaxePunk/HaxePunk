fb5651315a8cf0d7005b69cf7ae72980ac2c9e20
Matt Tuttle <heardtheword@gmail.com>
2011-02-21 15:13:30 -0600

    Sound fader classes are compiling

--- src/com/haxepunk/tweens/sound/Fader.hx
+++ src/com/haxepunk/tweens/sound/Fader.hx
@@ -1,48 +1,48 @@
-﻿package net.flashpunk.tweens.sound 
+﻿package com.haxepunk.tweens.sound;
+
+import com.haxepunk.HXP;
+import com.haxepunk.Tween;
+import com.haxepunk.utils.Ease;
+
+/**
+ * Global volume fader.
+ */
+class Fader extends Tween
 {
-	import net.flashpunk.FP;
-	import net.flashpunk.Tween;
+	/**
+	 * Constructor.
+	 * @param	complete	Optional completion callback.
+	 * @param	type		Tween type.
+	 */
+	public function new(complete:CompleteCallback = null, type:TweenType) 
+	{
+		super(0, type, complete);
+	}
 	
 	/**
-	 * Global volume fader.
+	 * Fades FP.volume to the target volume.
+	 * @param	volume		The volume to fade to.
+	 * @param	duration	Duration of the fade.
+	 * @param	ease		Optional easer function.
 	 */
-	public class Fader extends Tween
+	public function fadeTo(volume:Float, duration:Float, ease:EaseFunction = null)
 	{
-		/**
-		 * Constructor.
-		 * @param	complete	Optional completion callback.
-		 * @param	type		Tween type.
-		 */
-		public function Fader(complete:Function = null, type:uint = 0) 
-		{
-			super(0, type, complete);
-		}
-		
-		/**
-		 * Fades FP.volume to the target volume.
-		 * @param	volume		The volume to fade to.
-		 * @param	duration	Duration of the fade.
-		 * @param	ease		Optional easer function.
-		 */
-		public function fadeTo(volume:Number, duration:Number, ease:Function = null):void
-		{
-			if (volume < 0) volume = 0;
-			_start = FP.volume;
-			_range = volume - _start;
-			_target = duration;
-			_ease = ease;
-			start();
-		}
-		
-		/** @private Updates the Tween. */
-		override public function update():void 
-		{
-			super.update();
-			FP.volume = _start + _range * _t;
-		}
-		
-		// Fader information.
-		/** @private */ private var _start:Number;
-		/** @private */ private var _range:Number;
+		if (volume < 0) volume = 0;
+		_start = HXP.volume;
+		_range = volume - _start;
+		_target = duration;
+		_ease = ease;
+		start();
 	}
+	
+	/** @private Updates the Tween. */
+	override public function update() 
+	{
+		super.update();
+		HXP.volume = _start + _range * _t;
+	}
+	
+	// Fader information.
+	private var _start:Float;
+	private var _range:Float;
 }
--- src/com/haxepunk/tweens/sound/SfxFader.hx
+++ src/com/haxepunk/tweens/sound/SfxFader.hx
@@ -1,94 +1,95 @@
-﻿package net.flashpunk.tweens.sound 
+﻿package com.haxepunk.tweens.sound;
+
+import com.haxepunk.Sfx;
+import com.haxepunk.Tween;
+import com.haxepunk.utils.Ease;
+
+/**
+ * Sound effect fader.
+ */
+class SfxFader extends Tween
 {
-	import net.flashpunk.Sfx;
-	import net.flashpunk.Tween;
+	/**
+	 * Constructor.
+	 * @param	sfx			The Sfx object to alter.
+	 * @param	complete	Optional completion callback.
+	 * @param	type		Tween type.
+	 */
+	public function new(sfx:Sfx, complete:CompleteCallback = null, type:TweenType) 
+	{
+		super(0, type, finish);
+		_complete = complete;
+		_sfx = sfx;
+	}
 	
 	/**
-	 * Sound effect fader.
+	 * Fades the Sfx to the target volume.
+	 * @param	volume		The volume to fade to.
+	 * @param	duration	Duration of the fade.
+	 * @param	ease		Optional easer function.
 	 */
-	public class SfxFader extends Tween
+	public function fadeTo(volume:Float, duration:Float, ease:EaseFunction = null)
 	{
-		/**
-		 * Constructor.
-		 * @param	sfx			The Sfx object to alter.
-		 * @param	complete	Optional completion callback.
-		 * @param	type		Tween type.
-		 */
-		public function SfxFader(sfx:Sfx, complete:Function = null, type:uint = 0) 
-		{
-			super(0, type, finish);
-			_complete = complete;
-			_sfx = sfx;
-		}
-		
-		/**
-		 * Fades the Sfx to the target volume.
-		 * @param	volume		The volume to fade to.
-		 * @param	duration	Duration of the fade.
-		 * @param	ease		Optional easer function.
-		 */
-		public function fadeTo(volume:Number, duration:Number, ease:Function = null):void
-		{
-			if (volume < 0) volume = 0;
-			_start = _sfx.volume;
-			_range = volume - _start;
-			_target = duration;
-			_ease = ease;
-			start();
-		}
-		
-		/**
-		 * Fades out the Sfx, while also playing and fading in a replacement Sfx.
-		 * @param	play		The Sfx to play and fade in.
-		 * @param	loop		If the new Sfx should loop.
-		 * @param	duration	Duration of the crossfade.
-		 * @param	volume		The volume to fade in the new Sfx to.
-		 * @param	ease		Optional easer function.
-		 */
-		public function crossFade(play:Sfx, loop:Boolean, duration:Number, volume:Number = 1, ease:Function = null):void
-		{
-			_crossSfx = play;
-			_crossRange = volume;
-			_start = _sfx.volume;
-			_range = -_start;
-			_target = duration;
-			_ease = ease;
-			if (loop) _crossSfx.loop(0);
-			else _crossSfx.play(0);
-			start();
-		}
-		
-		/** @private Updates the Tween. */
-		override public function update():void 
-		{
-			super.update();
-			if (_sfx) _sfx.volume = _start + _range * _t;
-			if (_crossSfx) _crossSfx.volume = _crossRange * _t;
-		}
-		
-		/** @private When the tween completes. */
-		private function finish():void
-		{
-			if (_crossSfx)
-			{
-				if (_sfx) _sfx.stop();
-				_sfx = _crossSfx;
-				_crossSfx = null;
-			}
-			if (_complete != null) _complete();
-		}
-		
-		/**
-		 * The current Sfx this object is effecting.
-		 */
-		public function get sfx():Sfx { return _sfx; }
-		
-		// Fader information.
-		/** @private */ private var _sfx:Sfx;
-		/** @private */ private var _start:Number;
-		/** @private */ private var _range:Number;
-		/** @private */ private var _crossSfx:Sfx;
-		/** @private */ private var _crossRange:Number;
-		/** @private */ private var _complete:Function;
+		if (volume < 0) volume = 0;
+		_start = _sfx.volume;
+		_range = volume - _start;
+		_target = duration;
+		_ease = ease;
+		start();
 	}
+	
+	/**
+	 * Fades out the Sfx, while also playing and fading in a replacement Sfx.
+	 * @param	play		The Sfx to play and fade in.
+	 * @param	loop		If the new Sfx should loop.
+	 * @param	duration	Duration of the crossfade.
+	 * @param	volume		The volume to fade in the new Sfx to.
+	 * @param	ease		Optional easer function.
+	 */
+	public function crossFade(play:Sfx, loop:Bool, duration:Float, volume:Float = 1, ease:EaseFunction = null)
+	{
+		_crossSfx = play;
+		_crossRange = volume;
+		_start = _sfx.volume;
+		_range = -_start;
+		_target = duration;
+		_ease = ease;
+		if (loop) _crossSfx.loop(0);
+		else _crossSfx.play(0);
+		start();
+	}
+	
+	/** @private Updates the Tween. */
+	override public function update() 
+	{
+		super.update();
+		if (_sfx != null) _sfx.volume = _start + _range * _t;
+		if (_crossSfx != null) _crossSfx.volume = _crossRange * _t;
+	}
+	
+	/** @private When the tween completes. */
+	override public function finish()
+	{
+		if (_crossSfx != null)
+		{
+			if (_sfx != null) _sfx.stop();
+			_sfx = _crossSfx;
+			_crossSfx = null;
+		}
+		if (_complete != null) _complete();
+	}
+	
+	/**
+	 * The current Sfx this object is effecting.
+	 */
+	public var sfx(getSfx, null):Sfx;
+	private function getSfx():Sfx { return _sfx; }
+	
+	// Fader information.
+	private var _sfx:Sfx;
+	private var _start:Float;
+	private var _range:Float;
+	private var _crossSfx:Sfx;
+	private var _crossRange:Float;
+	private var _complete:CompleteCallback;
 }
