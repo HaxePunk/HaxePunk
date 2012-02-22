858783fc5c7cba90cdd9d38d23268b180f9601c2
Matt Tuttle <heardtheword@gmail.com>
2012-02-21 22:03:23 -0600

    Adding JoystickEvent stubs

--- src/com/haxepunk/utils/Input.hx
+++ src/com/haxepunk/utils/Input.hx
@@ -4,20 +4,23 @@ import flash.events.KeyboardEvent;
 import flash.events.MouseEvent;
 import flash.ui.Keyboard;
 import com.haxepunk.HXP;
+#if nme
+import nme.events.JoystickEvent;
+#end
 
 class Input
 {
-	
+
 	public static var keyString:String = "";
-	
+
 	public static var lastKey:Int;
-	
+
 	public static var mouseDown:Bool;
 	public static var mouseUp:Bool;
 	public static var mousePressed:Bool;
 	public static var mouseReleased:Bool;
 	public static var mouseWheel:Bool;
-	
+
 	/**
 	 * X position of the mouse on the screen.
 	 */
@@ -26,7 +29,7 @@ class Input
 	{
 		return HXP.screen.mouseX;
 	}
-	
+
 	/**
 	 * Y position of the mouse on the screen.
 	 */
@@ -35,7 +38,7 @@ class Input
 	{
 		return HXP.screen.mouseY;
 	}
-	
+
 	/**
 	 * The absolute mouse x position on the screen (unscaled).
 	 */
@@ -44,7 +47,7 @@ class Input
 	{
 		return Std.int(HXP.stage.mouseX);
 	}
-	
+
 	/**
 	 * The absolute mouse y position on the screen (unscaled).
 	 */
@@ -53,7 +56,7 @@ class Input
 	{
 		return Std.int(HXP.stage.mouseY);
 	}
-	
+
 	/**
 	 * Defines a new input.
 	 * @param	name		String to map the input to.
@@ -63,7 +66,7 @@ class Input
 	{
 		_control.set(name, keys);
 	}
-	
+
 	/**
 	 * If the input or key is held down.
 	 * @param	input		An input name or key to check for.
@@ -88,7 +91,7 @@ class Input
 		}
 		return input < 0 ? _keyNum > 0 : _key[input];
 	}
-	
+
 	/**
 	 * If the input or key was pressed this frame.
 	 * @param	input		An input name or key to check for.
@@ -108,7 +111,7 @@ class Input
 		}
 		return (input < 0) ? _pressNum != 0 : indexOf(_press, input) >= 0;
 	}
-	
+
 	/**
 	 * If the input or key was released this frame.
 	 * @param	input		An input name or key to check for.
@@ -128,7 +131,7 @@ class Input
 		}
 		return (input < 0) ? _releaseNum != 0 : indexOf(_release, input) >= 0;
 	}
-	
+
 	/**
 	 * Copy of Lambda.indexOf for speed/memory reasons
 	 * @param	a array to use
@@ -145,7 +148,7 @@ class Input
 		}
 		return -1;
 	}
-	
+
 	public static function enable()
 	{
 		if (!_enabled && HXP.stage != null)
@@ -155,9 +158,17 @@ class Input
 			HXP.stage.addEventListener(MouseEvent.MOUSE_DOWN, onMouseDown, false,  2);
 			HXP.stage.addEventListener(MouseEvent.MOUSE_UP, onMouseUp, false,  2);
 			HXP.stage.addEventListener(MouseEvent.MOUSE_WHEEL, onMouseWheel, false,  2);
+
+#if nme
+			HXP.stage.addEventListener(JoystickEvent.AXIS_MOVE, onJoyAxisMove);
+			HXP.stage.addEventListener(JoystickEvent.BALL_MOVE, onJoyBallMove);
+			HXP.stage.addEventListener(JoystickEvent.BUTTON_DOWN, onJoyButtonDown);
+			HXP.stage.addEventListener(JoystickEvent.BUTTON_UP, onJoyButtonUp);
+			HXP.stage.addEventListener(JoystickEvent.HAT_MOVE, onJoyHatMove);
+#end
 		}
 	}
-	
+
 	public static function update()
 	{
 		while (_pressNum-- > -1) _press[_pressNum] = -1;
@@ -167,11 +178,11 @@ class Input
 		if (mousePressed) mousePressed = false;
 		if (mouseReleased) mouseReleased = false;
 	}
-	
+
 	private static function onKeyDown(e:KeyboardEvent = null)
 	{
 		var code:Int = lastKey = e.keyCode;
-		
+
 		if (code == Key.BACKSPACE) keyString = keyString.substr(0, keyString.length - 1);
 		else if ((code > 47 && code < 58) || (code > 64 && code < 91) || code == 32)
 		{
@@ -183,7 +194,7 @@ class Input
 			#end
 			keyString += char;
 		}
-		
+
 		if (!_key[code])
 		{
 			_key[code] = true;
@@ -191,7 +202,7 @@ class Input
 			_press[_pressNum++] = code;
 		}
 	}
-	
+
 	private static function onKeyUp(e:KeyboardEvent = null)
 	{
 		var code:Int = e.keyCode;
@@ -202,7 +213,7 @@ class Input
 			_release[_releaseNum++] = code;
 		}
 	}
-	
+
 	private static function onMouseDown(e:MouseEvent)
 	{
 		if (!mouseDown)
@@ -212,22 +223,51 @@ class Input
 			mousePressed = true;
 		}
 	}
-	
+
 	private static function onMouseUp(e:MouseEvent)
 	{
 		mouseDown = false;
 		mouseUp = true;
 		mouseReleased = true;
 	}
-	
+
 	private static function onMouseWheel(e:MouseEvent)
 	{
 		mouseWheel = true;
 		_mouseWheelDelta = e.delta;
 	}
-	
+
+#if nme
+
+	private static function onJoyAxisMove(e:JoystickEvent)
+	{
+
+	}
+
+	private static function onJoyBallMove(e:JoystickEvent)
+	{
+
+	}
+
+	private static function onJoyButtonDown(e:JoystickEvent)
+	{
+
+	}
+
+	private static function onJoyButtonUp(e:JoystickEvent)
+	{
+
+	}
+
+	private static function onJoyHatMove(e:JoystickEvent)
+	{
+
+	}
+
+#end
+
 	private static inline var kKeyStringMax = 100;
-	
+
 	private static var _enabled:Bool = false;
 	private static var _key:Array<Bool> = new Array<Bool>();
 	private static var _keyNum:Int = 0;
