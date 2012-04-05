2a7b171806d739104e277aefb96a9fa23965486b
Matt Tuttle <heardtheword@gmail.com>
2012-04-05 14:31:56 -0500

    Updated mask test to include moveBy

--- tests/TestMasks.hx
+++ tests/TestMasks.hx
@@ -1,5 +1,8 @@
 import com.haxepunk.Mask;
 import com.haxepunk.Entity;
+import com.haxepunk.World;
+import com.haxepunk.Engine;
+import com.haxepunk.HXP;
 import com.haxepunk.masks.Circle;
 import com.haxepunk.masks.Hitbox;
 
@@ -8,12 +11,14 @@ class TestMasks extends haxe.unit.TestCase
 
 	public override function setup()
 	{
-		hitbox = new Hitbox(20, 20, -10, -10);
-		circle = new Circle(10);
+		var engine = new Engine(640, 480);
+		HXP.world = world = new World();
 
-		// have to assign the parents
-		hitbox.assignTo(new Entity(0, 0));
-		circle.assignTo(new Entity(0, 0));
+		hitbox = world.addMask(new Hitbox(20, 20, -10, -10), "box");
+		circle = world.addMask(new Circle(10), "circle");
+
+		// update entity lists
+		engine.update();
 	}
 
 	public override function tearDown()
@@ -24,12 +29,21 @@ class TestMasks extends haxe.unit.TestCase
 	public function testHitbox()
 	{
 		// check that we collide with the circle
-		assertTrue(hitbox.collide(circle));
+		assertTrue(hitbox.collide("circle",  0,  0) != null);
+		assertTrue(hitbox.collide("circle", 20, 20) == null);
 
-		circle.x = 20; circle.y = 20;
-		assertFalse(hitbox.collide(circle));
+		circle.x = 40; circle.y = 40; // move circle out of the way
+
+		// this shouldn't collide at all with the circle
+		hitbox.moveBy(20, 20, "circle", true);
+		assertTrue(hitbox.x == 20 && hitbox.y == 20);
+
+		// this should collide with the circle and move 20 to the left only
+		hitbox.moveBy(20, 20, "circle", true);
+		assertTrue(hitbox.x == 40 && hitbox.y == 20);
 	}
 
-	private var hitbox:Hitbox;
-	private var circle:Circle;
+	private var world:World;
+	private var hitbox:Entity;
+	private var circle:Entity;
 }
