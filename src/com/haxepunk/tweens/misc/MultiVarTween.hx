a845ee4789b4ae723962f0a8e74abe7a12fd014c
Matt Tuttle <heardtheword@gmail.com>
2012-07-05 13:49:08 -0500

    VarTween and MultiVarTween now support properties

--- src/com/haxepunk/tweens/misc/MultiVarTween.hx
+++ src/com/haxepunk/tweens/misc/MultiVarTween.hx
@@ -18,18 +18,18 @@ class MultiVarTween extends Tween
 		_vars = new Array<String>();
 		_start = new Array<Float>();
 		_range = new Array<Float>();
-		
+
 		super(0, type, complete);
 	}
-	
+
 	/**
 	 * Tweens multiple numeric public properties.
 	 * @param	object		The object containing the properties.
-	 * @param	values		An object containing key/value pairs of properties and target values.
+	 * @param	properties	An object containing key/value pairs of properties and target values.
 	 * @param	duration	Duration of the tween.
 	 * @param	ease		Optional easer function.
 	 */
-	public function tween(object:Dynamic, values:Dynamic, duration:Float, ease:EaseFunction = null)
+	public function tween(object:Dynamic, properties:Dynamic, duration:Float, ease:EaseFunction = null)
 	{
 		_object = object;
 		HXP.clear(_vars);
@@ -38,25 +38,44 @@ class MultiVarTween extends Tween
 		_target = duration;
 		_ease = ease;
 		var p:String;
-		var fields:Array<String> = Reflect.fields(values);
+
+		var fields:Array<String> = null;
+		if (Reflect.isObject(properties))
+		{
+			fields = Reflect.fields(properties);
+		}
+		else
+		{
+			throw "Unsupported MultiVar properties container - use Object containing key/value pairs.";
+		}
+
 		for (p in fields)
 		{
-			if (!Reflect.hasField(object, p)) throw "The Object does not have the property\"" + p + "\", or it is not accessible.";
-			var a:Float = Reflect.field(object, p);
-			if (a == 0) throw "The property \"" + p + "\" is not numeric.";
+			var a:Float = Reflect.getProperty(object, p);
+
+#if (cpp || neko)
+			if (a == null)
+			{
+				throw "The Object does not have the property \"" + p + "\", or it is not accessible.";
+			}
+#end
+			if (Math.isNaN(a))
+			{
+				throw "The property \"" + p + "\" is not numeric.";
+			}
 			_vars.push(p);
 			_start.push(a);
-			_range.push(Reflect.field(values, p) - a);
+			_range.push(Reflect.field(properties, p) - a);
 		}
 		start();
 	}
-	
+
 	/** @private Updates the Tween. */
 	override public function update()
 	{
 		super.update();
 		var i:Int = _vars.length;
-		while (i-- > 0) Reflect.setField(_object, _vars[i], _start[i] + _range[i] * _t);
+		while (i-- > 0) Reflect.setProperty(_object, _vars[i], _start[i] + _range[i] * _t);
 	}
 
 	// Tween information.
