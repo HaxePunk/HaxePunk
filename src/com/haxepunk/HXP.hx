541701d754d2b1e0e8af871ebd6e93e151d21935
Matt Tuttle <heardtheword@gmail.com>
2012-10-01 09:30:37 -0500

    Updated CHANGELOG and version number to 1.6.7

--- src/com/haxepunk/HXP.hx
+++ src/com/haxepunk/HXP.hx
@@ -31,7 +31,7 @@ class HXP
 	/**
 	 * The HaxePunk major version.
 	 */
-	public static inline var VERSION:String = "1.6.6";
+	public static inline var VERSION:String = "1.6.7";
 
 	/**
 	 * The standard layer used since only flash can handle negative indicies in arrays, set your layers to some offset of this
