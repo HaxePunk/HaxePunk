54a5b51c5dcd843071f972123b313f7073874cc9
Matt Tuttle <heardtheword@gmail.com>
2013-01-03 14:45:21 -0600

    Simplifying template class

--- template/src/{{PROJECT_CLASS}}.hx
+++ template/src/{{PROJECT_CLASS}}.hx
@@ -4,17 +4,6 @@ import com.haxepunk.HXP;
 class {{PROJECT_CLASS}} extends Engine
 {
 
-	public static inline var kScreenWidth:Int = {{WIDTH}};
-	public static inline var kScreenHeight:Int = {{HEIGHT}};
-	public static inline var kFrameRate:Int = {{FRAMERATE}};
-	public static inline var kClearColor:Int = 0x333333;
-	public static inline var kProjectName:String = "{{PROJECT_NAME}}";
-
-	public function new()
-	{
-		super(kScreenWidth, kScreenHeight, kFrameRate, false);
-	}
-
 	override public function init()
 	{
 #if debug
@@ -25,13 +14,7 @@ class {{PROJECT_CLASS}} extends Engine
 			HXP.console.enable();
 		}
 #end
-		HXP.screen.color = kClearColor;
-//		HXP.world = new YourWorld();
-	}
-
-	public static function main()
-	{
-		new {{PROJECT_CLASS}}();
+		HXP.world = new YourWorld();
 	}
 
 }
