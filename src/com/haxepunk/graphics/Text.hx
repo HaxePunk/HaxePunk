ff7bdb8f16a967bb3bb05d86c4876a1b02bd45f5
Matt Tuttle <heardtheword@gmail.com>
2013-03-12 10:08:52 -0500

    Fixing Text on hardware targets

--- src/com/haxepunk/graphics/Text.hx
+++ src/com/haxepunk/graphics/Text.hx
@@ -1,6 +1,7 @@
 package com.haxepunk.graphics;
 
 import nme.display.BitmapData;
+import nme.display.Sprite;
 import nme.geom.Point;
 import nme.geom.Rectangle;
 import nme.text.TextField;
@@ -9,6 +10,8 @@ import nme.text.TextFormatAlign;
 
 import com.haxepunk.HXP;
 import com.haxepunk.Graphic;
+import com.haxepunk.graphics.atlas.Atlas;
+import com.haxepunk.graphics.atlas.AtlasRegion;
 
 typedef TextOptions = {
 	@:optional var font:String;
@@ -30,6 +33,10 @@ typedef TextOptions = {
 class Text extends Image
 {
 
+	public var resizable:Bool;
+	public var textWidth(default, null):Int;
+	public var textHeight(default, null):Int;
+
 	/**
 	 * Constructor.
 	 * @param text    Text to display.
@@ -39,7 +46,7 @@ class Text extends Image
 	 * @param height  Image height (leave as 0 to size to the starting text string).
 	 * @param options An object containing optional parameters contained in TextOptions
 	 */
-	public function new(text:String, ?x:Float = 0, ?y:Float = 0, ?width:Int = 0, ?height:Int = 0, ?options:TextOptions = null)
+	public function new(text:String, x:Float = 0, y:Float = 0, width:Int = 0, height:Int = 0, ?options:TextOptions)
 	{
 		if (options == null)
 		{
@@ -49,27 +56,19 @@ class Text extends Image
 
 		if (options.font == null)  options.font = HXP.defaultFont;
 		if (options.size == 0)     options.size = 16;
-#if (flash || js)
 		if (options.align == null) options.align = TextFormatAlign.LEFT;
-#else
-		if (options.align == null) options.align = "left";
-#end
 
-#if nme
 		var fontObj = nme.Assets.getFont(options.font);
-		_form = new TextFormat(fontObj.fontName, options.size, options.color);
-#else
-		_form = new TextFormat(options.font, options.size, options.color);
-#end
-		_form.align = options.align;
-		_form.leading = options.leading;
+		_format = new TextFormat(fontObj.fontName, options.size, options.color);
+		_format.align = options.align;
+		_format.leading = options.leading;
 
 		_field = new TextField();
 #if flash
 		_field.embedFonts = true;
 #end
 		_field.wordWrap = options.wordWrap;
-		_field.defaultTextFormat = _form;
+		_field.defaultTextFormat = _format;
 		_field.text = text;
 
 		resizable = options.resizable;
@@ -77,7 +76,19 @@ class Text extends Image
 		if (width == 0) width = Std.int(_field.textWidth + 4);
 		if (height == 0) height = Std.int(_field.textHeight + 4);
 
-		super(HXP.createBitmap(width, height, true));
+		var source:Dynamic;
+		if (HXP.renderMode.has(RenderMode.HARDWARE))
+		{
+			HXP.rect.x = HXP.rect.y = 0;
+			HXP.rect.width = _field.width;
+			HXP.rect.height = _field.height;
+			source = new AtlasRegion(null, 0, HXP.rect);
+		}
+		else
+		{
+			source = HXP.createBitmap(width, height, true);
+		}
+		super(source);
 
 		this.text = text;
 		this.x = x;
@@ -87,46 +98,76 @@ class Text extends Image
 	/** @private Updates the drawing buffer. */
 	public override function updateBuffer(clearBefore:Bool = false)
 	{
-		_field.setTextFormat(_form);
+		_field.setTextFormat(_format);
 
-		_field.width = width;
+		if (_blit) _field.width = _bufferRect.width;
 		_field.width = textWidth = Math.ceil(_field.textWidth + 4);
 		_field.height = textHeight = Math.ceil(_field.textHeight + 4);
 
-		if (resizable)
+		if (_blit)
 		{
-			_bufferRect.width = textWidth;
-			_bufferRect.height = textHeight;
+			if (resizable)
+			{
+				_bufferRect.width = textWidth;
+				_bufferRect.height = textHeight;
+			}
+
+			if (textWidth > _source.width || textHeight > _source.height)
+			{
+				_source = HXP.createBitmap(
+					Std.int(Math.max(textWidth, _source.width)),
+					Std.int(Math.max(textHeight, _source.height)),
+					true);
+
+				_sourceRect = _source.rect;
+				createBuffer();
+			}
+			else
+			{
+				_source.fillRect(_sourceRect, HXP.blackColor);
+			}
+
+			if (resizable)
+			{
+				_field.width = textWidth;
+				_field.height = textHeight;
+			}
+
+			_source.draw(_field);
+			super.updateBuffer(clearBefore);
 		}
+	}
 
-		if (textWidth > _source.width || textHeight > _source.height)
+	public override function render(target:BitmapData, point:Point, camera:Point)
+	{
+		if (_blit)
 		{
-			_source = HXP.createBitmap(
-				Std.int(Math.max(textWidth, _source.width)),
-				Std.int(Math.max(textHeight, _source.height)),
-				true);
-
-			_sourceRect = _source.rect;
-			createBuffer();
+			super.render(target, point, camera);
 		}
 		else
 		{
-			_source.fillRect(_sourceRect, HXP.blackColor);
+			_field.x = point.x + x - originX - camera.x * scrollX;
+			_field.y = point.y + y - originY - camera.y * scrollY;
 		}
-
-		if (resizable)
-		{
-			_field.width = textWidth;
-			_field.height = textHeight;
-		}
-
-		cast(_source, BitmapData).draw(_field);
-		super.updateBuffer(clearBefore);
 	}
 
-	public var resizable:Bool;
-	public var textWidth(default, null):Int;
-	public var textHeight(default, null):Int;
+	/**
+	 * Moves the TextField to the correct sprite layer
+	 */
+	private override function setLayer(value:Int):Int
+	{
+		if (value == layer) return value;
+		if (_blit == false)
+		{
+			if (_parent != null)
+			{
+				_parent.removeChild(_field);
+			}
+			_parent = Atlas.getSpriteByLayer(value);
+			_parent.addChild(_field);
+		}
+		return super.setLayer(value);
+	}
 
 	/**
 	 * Text string.
@@ -137,7 +178,7 @@ class Text extends Image
 		if (text == value) return value;
 		_field.text = text = value;
 		updateBuffer();
-		return text;
+		return value;
 	}
 
 	/**
@@ -150,9 +191,9 @@ class Text extends Image
 #if nme
 		value = nme.Assets.getFont(value).fontName;
 #end
-		_form.font = font = value;
+		_format.font = font = value;
 		updateBuffer();
-		return font;
+		return value;
 	}
 
 	/**
@@ -162,12 +203,13 @@ class Text extends Image
 	private function setSize(value:Int):Int
 	{
 		if (size == value) return value;
-		_form.size = size = value;
+		_format.size = size = value;
 		updateBuffer();
 		return value;
 	}
 
 	// Text information.
 	private var _field:TextField;
-	private var _form:TextFormat;
+	private var _format:TextFormat;
+	private var _parent:Sprite;
 }
