0ee34dd99bf849fa74566747088ec92f914b2caa
Matt Tuttle <heardtheword@gmail.com>
2013-05-31 07:07:32 -0500

    added Xbox button configuration for Mac

--- com/haxepunk/utils/Joystick.hx
+++ com/haxepunk/utils/Joystick.hx
@@ -124,6 +124,7 @@ class Joystick
 
 class XBOX_GAMEPAD
 {
+#if windows
 	public static var A_BUTTON:Int = 0;
 	public static var B_BUTTON:Int = 1;
 	public static var X_BUTTON:Int = 2;
@@ -143,4 +144,26 @@ class XBOX_GAMEPAD
 	* Keep in mind that if TRIGGER axis returns value > 0 then LT is being pressed, and if it's < 0 then RT is being pressed
 	*/
 	public static var TRIGGER:Int = 2;
+#elif mac
+	public static var A_BUTTON:Int = 11;
+	public static var B_BUTTON:Int = 12;
+	public static var X_BUTTON:Int = 13;
+	public static var Y_BUTTON:Int = 14;
+	public static var LB_BUTTON:Int = 8;
+	public static var RB_BUTTON:Int = 9;
+	public static var BACK_BUTTON:Int = 5;
+	public static var START_BUTTON:Int = 4;
+	public static var LEFT_ANALOGUE_BUTTON:Int = 6;
+	public static var RIGHT_ANALOGUE_BUTTON:Int = 7;
+	public static var LEFT_ANALOGUE_X:Int = 0;
+	public static var LEFT_ANALOGUE_Y:Int = 1;
+	public static var RIGHT_ANALOGUE_X:Int = 2;
+	public static var RIGHT_ANALOGUE_Y:Int = 3;
+	public static var DPAD_UP:Int = 0;
+	public static var DPAD_DOWN:Int = 1;
+	public static var DPAD_LEFT:Int = 2;
+	public static var DPAD_RIGHT:Int = 3;
+
+	//public static var TRIGGER:Int = 3;
+#end
 }
