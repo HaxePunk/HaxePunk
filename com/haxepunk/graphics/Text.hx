5a730e80cbe0e717e873b55f2b27703aeff68743
Valentin Lemiere <valent.lemiere@gmail.com>
2013-07-05 18:36:31 +0200

    Fix text alpha on hardware rendermode.

--- com/haxepunk/graphics/Text.hx
+++ com/haxepunk/graphics/Text.hx
@@ -262,6 +262,21 @@ class Text extends Image
 		_parent = _entity.scene.getSpriteByLayer(layer);
 		_parent.addChild(_field);
 	}
+	
+	/**
+	 * Alpha of the text.
+	 */
+	override function set_alpha(value:Float):Float
+	{
+		if (_blit)
+		{
+			return super.set_alpha(value);
+		}
+		else
+		{
+			return _field.alpha = value;
+		}
+	}
 
 	// Text information.
 	private var _field:TextField;
