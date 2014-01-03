7f63a14d5e2ad0d7d46b57c8f9dbdc4d95659b9d
bendmorris <ben@bendmorris.com>
2014-01-02 23:02:22 -0500

    Adding support for bitmap font rendering. Currently supports Starling XML format. Only works on HW rendering mode.

---
+++ com/haxepunk/graphics/BitmapText.hx
@@ -0,0 +1,132 @@
+package com.haxepunk.graphics;
+
+import flash.display.BitmapData;
+import flash.geom.Point;
+import flash.geom.Rectangle;
+import com.haxepunk.HXP;
+import com.haxepunk.RenderMode;
+import com.haxepunk.Graphic;
+import com.haxepunk.graphics.Canvas;
+import com.haxepunk.graphics.Text;
+import com.haxepunk.graphics.atlas.BitmapFontAtlas;
+
+
+class BitmapText extends Graphic {
+	private var _set:BitmapData;
+	private var _font:BitmapFontAtlas;
+	
+	public var width:Float=0;
+	public var height:Float=0;
+	
+	public var scale:Float=1;
+	public var scaleX:Float=1;
+	public var scaleY:Float=1;
+	
+	public var lineSpacing:Int=0;
+	public var charSpacing:Int=0;
+	
+	public var text(default, set):String;
+	private var _lines:Array<String>;
+	
+	public function new(text:String, x:Float=0, y:Float=0, width:Float=0, height:Float=0, ?options:TextOptions) {
+		super();
+		
+		var font = BitmapFontAtlas.getFont(options.font);
+		
+		// load the font
+		if(Std.is(font, BitmapFontAtlas)) {
+			_blit = false;
+			_font = cast(font, BitmapFontAtlas);
+		}
+		
+		// failure to load
+		if (_set == null && _font == null)
+			throw "Invalid font glyphs provided.";
+		
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		
+		var color = options.color;
+		this.color = color;
+		
+		this.text = text;
+	}
+	
+	private var _red:Float;
+	private var _green:Float;
+	private var _blue:Float;
+	public var color(default, set_color):Int;
+	private function set_color(value:Int):Int {
+		value &= 0xFFFFFF;
+		if (color == value) return value;
+		// save individual color channel values
+		_red = HXP.getRed(value) / 255;
+		_green = HXP.getGreen(value) / 255;
+		_blue = HXP.getBlue(value) / 255;
+
+		return color = value;
+	}
+	
+	public var alpha:Float=1;
+	
+	public function set_text(text:String) {
+		this.text = text;
+		_lines = text.split("\n");
+		return text;
+	}
+	
+	override public function render(target:BitmapData, point:Point, camera:Point) {
+		var sx = scale * scaleX,
+			sy = scale * scaleY;
+		
+		// determine drawing location
+		_point.x = point.x + x - camera.x * scrollX;
+		_point.y = point.y + y - camera.y * scrollY;
+		
+		var fsx = HXP.screen.fullScaleX,
+			fsy = HXP.screen.fullScaleY;
+		
+		_point.x = Math.floor(_point.x * fsx);
+		_point.y = Math.floor(_point.y * fsy);
+		
+		var lineHeight:Int = Std.int((_font.lineHeight + lineSpacing) * sy*fsy);
+		
+		var y:Int = 0;
+		var x:Int = 0;
+		var rx:Int = 0;
+		var ry:Int = 0;
+		for (y in 0 ... _lines.length) {
+			var line = _lines[y];
+			
+			for (x in 0 ... line.length) {
+				var letter = line.charAt(x);
+				var region = _font.getChar(letter);
+				var md = _font.glyphMetadata[letter];
+				
+				if (letter==' ') {
+					// it's a space, just move the cursor
+					rx += Std.int(md.xAdvance * sx*fsx);
+				} else {
+					// draw the character
+					region.draw(_point.x+rx+md.xOffset*sx*fsx, 
+					            _point.y+ry+md.yOffset*sy*fsy, 
+					            layer,
+					            sx * fsx, sy * fsy, 0,
+					            _red, _green, _blue, alpha);
+					
+					// advance cursor position
+					rx += Std.int((md.xAdvance + charSpacing) * sx*fsx);
+					if (width != 0 && rx > width*sx*fsx) {
+						rx = 0;
+						ry += lineHeight;
+					}
+				}
+			}
+			
+			rx = 0;
+			ry += lineHeight;
+		}
+	}
+}
---
+++ com/haxepunk/graphics/atlas/BitmapFontAtlas.hx
@@ -0,0 +1,80 @@
+package com.haxepunk.graphics.atlas;
+
+import openfl.Assets;
+import haxe.xml.Fast;
+
+
+enum BitmapFontFormat {
+	XML;
+}
+
+typedef GlyphMetadata = {
+	var xOffset:Int;
+	var yOffset:Int;
+	var xAdvance:Int;
+};
+
+class BitmapFontAtlas extends TextureAtlas {
+	private static var _fonts:Map<String, BitmapFontAtlas>;
+	
+	public static function getFont(fontName:String, format:BitmapFontFormat=null):BitmapFontAtlas {
+		if (_fonts == null) _fonts = new Map();
+		
+		if (format == null) format = XML;
+		
+		if (!_fonts.exists(fontName)) {
+			_fonts[fontName] = switch(format) {
+				case XML: BitmapFontAtlas.loadXMLFont(fontName);
+			}
+		}
+		
+		return _fonts[fontName];
+	}
+	
+	public static function loadXMLFont(file:String):BitmapFontAtlas {
+		var atlas = new BitmapFontAtlas(StringTools.replace(file, ".fnt", ".png"));
+		
+		var xml = Xml.parse(Assets.getText(file));
+		var fast = new Fast(xml.firstElement());
+		
+		atlas.lineHeight = Std.parseInt(fast.node.common.att.lineHeight);
+		var chars = fast.node.chars;
+		
+		for (char in chars.nodes.char) {
+			HXP.rect.x = Std.parseInt(char.att.x);
+			HXP.rect.y = Std.parseInt(char.att.y);
+			HXP.rect.width = Std.parseInt(char.att.width);
+			HXP.rect.height = Std.parseInt(char.att.height);
+			
+			var glyph = char.att.letter;
+			glyph = glyph=="space" ? ' ' : glyph;
+			
+			var md:GlyphMetadata = {
+			                        xOffset:char.has.xoffset ? Std.parseInt(char.att.xoffset) : 0,
+			                        yOffset:char.has.yoffset ? Std.parseInt(char.att.yoffset) : 0,
+			                        xAdvance:char.has.xadvance ? Std.parseInt(char.att.xadvance) : 0
+			                        };
+			
+			// set the defined region
+			var region = atlas.defineRegion(glyph, HXP.rect);
+			atlas.glyphMetadata[glyph] = md;
+		}
+		return atlas;
+	}
+	
+	private function new(source:Dynamic) {
+		super(source);
+		glyphMetadata = new Map();
+	}
+	
+	public var lineHeight:Int = 0;
+	public var glyphMetadata:Map<String, GlyphMetadata>;
+	
+	public function getChar(name:String):AtlasRegion {
+		try {
+			return getRegion(name);
+		} catch(msg:String) {
+			return getRegion(' ');
+		}
+	}
+}
