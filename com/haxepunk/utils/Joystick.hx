0287376442c89e80e912f56fc556e03bdc67e2c7
Nananas <nananaske@hotmail.com>
2014-03-03 11:35:17 +0100

    Ouya controller mapping for desktop
    Keycodes change when connecting an Ouya controller to Windows neko and cpp (can't check mac).

--- com/haxepunk/utils/Joystick.hx
+++ com/haxepunk/utils/Joystick.hx
@@ -160,6 +160,7 @@ class Joystick
 
 class OUYA_GAMEPAD
 {
+#if ouya	// ouya console mapping
 	// buttons
 	public static inline var O_BUTTON:Int = 0; // 96
 	public static inline var U_BUTTON:Int = 3; // 99
@@ -193,6 +194,37 @@ class OUYA_GAMEPAD
 	public static inline var RIGHT_ANALOGUE_Y:Int = 14;
 	public static inline var LEFT_TRIGGER:Int = 17;
 	public static inline var RIGHT_TRIGGER:Int = 18;
+	
+#else	// desktop mapping
+	public static inline var O_BUTTON:Int = 0;
+	public static inline var U_BUTTON:Int = 1;
+	public static inline var Y_BUTTON:Int = 2;
+	public static inline var A_BUTTON:Int = 3;
+	public static inline var LB_BUTTON:Int = 4;
+	public static inline var RB_BUTTON:Int = 5;
+	public static inline var BACK_BUTTON:Int = 20; // no back button!
+	public static inline var START_BUTTON:Int = 20; // no start button!
+	public static inline var LEFT_ANALOGUE_BUTTON:Int = 6;
+	public static inline var RIGHT_ANALOGUE_BUTTON:Int = 7;
+	public static inline var LEFT_TRIGGER_BUTTON:Int = 12;
+	public static inline var RIGHT_TRIGGER_BUTTON:Int = 13;
+	public static inline var DPAD_UP:Int = 8;
+	public static inline var DPAD_DOWN:Int = 9;
+	public static inline var DPAD_LEFT:Int = 10;
+	public static inline var DPAD_RIGHT:Int = 11;
+	
+	/**
+	 * The Home button only works on the Ouya-console
+	 */
+	public static inline var HOME_BUTTON:Int = 16777234;
+
+	public static inline var LEFT_ANALOGUE_X:Int = 0;
+	public static inline var LEFT_ANALOGUE_Y:Int = 1;
+	public static inline var RIGHT_ANALOGUE_X:Int = 5;
+	public static inline var RIGHT_ANALOGUE_Y:Int = 4;
+	public static inline var LEFT_TRIGGER:Int = 2;	// negative values before button trigger, positive values after
+	public static inline var RIGHT_TRIGGER:Int = 3;	// negative values before button trigger, positive values after
+#end
 }
 
 class XBOX_GAMEPAD
