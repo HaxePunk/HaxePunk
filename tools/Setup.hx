60fb60c3ec523ba34cc0b19b6466e82cdc5ec6fc
Valentin Lemière <me@ibilon.com>
2014-03-05 11:13:14 +0100

    Add update command

--- tools/Setup.hx
+++ tools/Setup.hx
@@ -2,13 +2,25 @@ import com.haxepunk.utils.HaxelibInfo;
 
 class Setup
 {
-	public static function installDependencies ()
+	public static function setup()
+	{
+		installDependencies();
+		Sys.command("haxelib run $OPENFL setup");
+	}
+
+	public static function update()
+	{
+		Sys.command("haxelib update HaxePunk");
+		installDependencies();
+	}
+
+	public static function installDependencies()
 	{
 		for (field in Reflect.fields(HaxelibInfo.install))
 		{
 			Sys.command("haxelib install " + field + " " + Reflect.field(HaxelibInfo.install, field));
 		}
-		
-		Sys.command("haxelib run lime setup");
 	}
+
+	private static inline var OPENFL = "lime";
 }
