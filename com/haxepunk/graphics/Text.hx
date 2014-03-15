587cc7f0fe5442b00737cce3a30206bd248a5b9f
Valentin Lemière <me@ibilon.com>
2014-03-15 16:20:06 +0100

    Fix text and richText color change, Fix #261

--- com/haxepunk/graphics/Text.hx
+++ com/haxepunk/graphics/Text.hx
@@ -301,18 +301,6 @@ class Text extends Image
 	}
 
 	/**
-	 * Override the tinting values for atlas
-	 */
-	override private function set_color(value:Int):Int
-	{
-		value &= 0xFFFFFF;
-		if (_color == value) return value;
-		_color = value;
-		if (blit) updateColorTransform();
-		return _color;
-	}
-
-	/**
 	 * Text string.
 	 */
 	public var text(get, set):String;
@@ -347,6 +335,7 @@ class Text extends Image
 		if (fromPlain && _richText != null)
 		{
 			_format.color = 0xFFFFFFFF;
+			_red = _green = _blue = 1;
 			updateColorTransform();
 		}
 		else
