1d46d12a54965992169e9b25882d1ce352281725
Matt Tuttle <heardtheword@gmail.com>
2014-03-29 10:14:42 -0500

    Fixing position for Entity

--- com/haxepunk/HXP.hx
+++ com/haxepunk/HXP.hx
@@ -28,7 +28,21 @@ import haxe.CallStack;
 import haxe.EnumFlags;
 import haxe.Timer;
 
-typedef Position = {x:Float, y:Float};
+abstract Position ({x:Float, y:Float})
+{
+	private function new(obj:Dynamic) this = obj;
+
+	public var x(get, set):Float;
+	private inline function get_x():Float return this.x;
+	private inline function set_x(value:Float):Float return this.x = value;
+
+	public var y(get, set):Float;
+	private inline function get_y():Float return this.y;
+	private inline function set_y(value:Float):Float return this.y = value;
+
+	@:from public static inline function fromObject(obj:{x:Float, y:Float}) return new Position(obj);
+	@:from public static inline function fromEntity(entity:Entity) return new Position(entity);
+}
 
 /**
  * Static catch-all class used to access global properties and functions.
