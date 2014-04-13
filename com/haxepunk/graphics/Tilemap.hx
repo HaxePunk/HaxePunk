2b8c81d1f42b5860a033415ed3ed6795942af261
bendmorris <ben@bendmorris.com>
2014-04-13 12:59:04 -0400

    Adjust scaling of individual tiles to ensure no gaps or overlaps.

--- com/haxepunk/graphics/Tilemap.hx
+++ com/haxepunk/graphics/Tilemap.hx
@@ -461,18 +461,20 @@ class Tilemap extends Canvas
 			stepy:Float = th * scy * scaley,
 			tile:Int = 0;
 
-		// adjust scale to fill gaps
-		scx = stepx / tileWidth;
-		scy = stepy / tileHeight;
-
 		for (y in starty...desty)
 		{
 			wx = sx;
+			// ensure no vertical overlap between this and next tile
+			scy = (Math.floor(wy+stepy) - Math.floor(wy)) / tileHeight;
+
 			for (x in startx...destx)
 			{
 				tile = _map[y % _rows][x % _columns];
 				if (tile >= 0)
 				{
+					// ensure no horizontal overlap between this and next tile
+					scx = (Math.floor(wx+stepx) - Math.floor(wx)) / tileWidth;
+
 					_atlas.prepareTile(tile, wx, wy, layer, scx, scy, 0, _red, _green, _blue, alpha);
 				}
 				wx += stepx;
