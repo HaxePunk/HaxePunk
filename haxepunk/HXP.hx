8d33286c396d5bb3fdc044f61819ce741d97404c
Valentin Lemière <me@ibilon.com>
2014-05-31 12:02:26 +0200

    Fix lib name, Fix neko build issue (uninitialized variables\!)

--- haxepunk/HXP.hx
+++ haxepunk/HXP.hx
@@ -2,6 +2,6 @@ package haxepunk;
 
 class HXP
 {
-	public static var windowWidth:Int;
-	public static var windowHeight:Int;
+	public static var windowWidth:Int = 0;
+	public static var windowHeight:Int = 0;
 }
