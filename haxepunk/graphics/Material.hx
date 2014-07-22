e2caab6da93636a9a841747b364314aa00a6bc25
Matt Tuttle <heardtheword@gmail.com>
2014-07-22 00:22:27 -0500

    Adding basic material loading from text file

--- haxepunk/graphics/Material.hx
+++ haxepunk/graphics/Material.hx
@@ -4,6 +4,8 @@ import haxepunk.math.Matrix4;
 import haxepunk.renderers.Renderer;
 import lime.Assets;
 
+using StringTools;
+
 class Material
 {
 
@@ -15,8 +17,80 @@ class Material
 
 		// set a default shader if none is given
 		this.shader = (shader == null ? _defaultShader : shader);
+		// fromText(Assets.getText("assets/player.material"));
 
 		_modelViewMatrixUniform = this.shader.uniform("uMatrix");
+	}
+
+	public function fromText(text:String)
+	{
+		var buffer = "";
+		var tokens = new Array<String>();
+		for (i in 0...text.length)
+		{
+			var c = text.charAt(i);
+			if (c == ' ' || c == '\n' || c == '\r' || c == '\t')
+			{
+				if (buffer != "")
+				{
+					tokens.push(buffer);
+					buffer = "";
+				}
+				continue;
+			}
+			buffer += c;
+		}
+		if (buffer != "")
+		{
+			tokens.push(buffer);
+		}
+
+		try {
+			loadMaterial(tokens);
+		} catch (e:String) {
+			trace(e);
+		}
+	}
+
+	private inline function expected(tokens:Array<String>, expected:String):String
+	{
+		var token = tokens.shift();
+		if (token != expected) throw 'Expected $expected but got "$token"';
+		return token;
+	}
+
+	private function loadMaterial(tokens:Array<String>)
+	{
+		switch (tokens.shift())
+		{
+			case "material":
+				var materialName = tokens.shift();
+				expected(tokens, "{");
+				loadMaterial(tokens);
+				expected(tokens, "}");
+			case "technique":
+				expected(tokens, "{");
+				loadMaterial(tokens);
+				expected(tokens, "}");
+			case "texture_unit":
+				expected(tokens, "{");
+				loadMaterial(tokens);
+				expected(tokens, "}");
+			case "pass":
+				expected(tokens, "{");
+				loadMaterial(tokens);
+				expected(tokens, "}");
+			case "texture":
+				var texture = tokens.shift();
+				if (Assets.exists(texture))
+				{
+					addTexture(Texture.fromAsset(texture));
+				}
+				else
+				{
+					throw 'Texture "$texture" does not exist';
+				}
+		}
 	}
 
 	public function addTexture(texture:Texture, uniformName:String="uImage0")
