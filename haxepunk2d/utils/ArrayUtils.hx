96ec781d613abdec355ccd3eea8e20e569d7f9cf
Marco <Marc010@users.noreply.github.com>
2015-01-03 16:27:46 +0100

    add @:generic again

--- haxepunk2d/utils/ArrayUtils.hx
+++ haxepunk2d/utils/ArrayUtils.hx
@@ -8,20 +8,24 @@ class ArrayUtils // to be used with "using"
 	/**
 	 * Returns the previous element in the array relative to the current element.
 	 */
+  @:generic
 	static function previous<T> (array:Array<T>, current:T, loop:Bool = true):T;
 
 	/**
 	 * Returns the next element in the array relative to the current element.
 	 */
+  @:generic
 	static function next<T> (array:Array<T>, current:T, loop:Bool = true):T;
 	
 	/**
 	 * Empties an array.
 	 */
+  @:generic
 	static function clear<T>(array:Array<T>):T;
 	
 	/**
 	 * Checks if the array contains the specified element using standard equality.
 	 */
+  @:generic
 	static function contains<T>(array:Array<T>, element:T):Bool;
 }
