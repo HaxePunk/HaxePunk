fb33b802dc4fdc3e5f3fb829946d91785eedd836
Marco <Marc010@users.noreply.github.com>
2015-01-03 16:25:09 +0100

    Add clear() and contains() to ArrayUtils

--- haxepunk2d/utils/ArrayUtils.hx
+++ haxepunk2d/utils/ArrayUtils.hx
@@ -8,12 +8,20 @@ class ArrayUtils // to be used with "using"
 	/**
 	 * Returns the previous element in the array relative to the current element.
 	 */
-	@:generic
 	static function previous<T> (array:Array<T>, current:T, loop:Bool = true):T;
 
 	/**
 	 * Returns the next element in the array relative to the current element.
 	 */
-	@:generic
 	static function next<T> (array:Array<T>, current:T, loop:Bool = true):T;
+	
+	/**
+	 * Empties an array.
+	 */
+	static function clear<T>(array:Array<T>):T;
+	
+	/**
+	 * Checks if the array contains the specified element using standard equality.
+	 */
+	static function contains<T>(array:Array<T>, element:T):Bool;
 }
