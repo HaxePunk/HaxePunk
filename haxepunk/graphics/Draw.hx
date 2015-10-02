1e98303f488b70fed944d3b1eafa83f0ac011c9e
MattTuttle <heardtheword@gmail.com>
2015-10-02 16:22:07 -0500

    Adding grid method to Draw class

--- haxepunk/graphics/Draw.hx
+++ haxepunk/graphics/Draw.hx
@@ -115,6 +115,32 @@ class Draw
 		SpriteBatch.addRectIndices();
 	}
 
+	/**
+	 * Draws a grid to the screen
+	 * @param gx x-axis value of the grid
+	 * @param gy y-axis value of the grid
+	 * @param gw width of the grid
+	 * @param gh height of the grid
+	 * @param color the color of the grid lines
+	 */
+	public static function grid(gx:Float, gy:Float, gw:Float, gh:Float, cellX:Int, cellY:Int, color:Color)
+    {
+        var offset = gx,
+            step = gw / cellX; // horizontal offset
+        for (i in 0...cellX+1)
+        {
+            Draw.line(offset, gy, offset, gy + gh, color);
+            offset += step;
+        }
+        offset = gy; // vertical offset
+        step = gh / cellY;
+        for (i in 0...cellY+1)
+        {
+            Draw.line(gx, offset, gx + gw, offset, color);
+            offset += step;
+        }
+    }
+
 	private static var _vIndex:Int = 0;
 	private static var _iIndex:Int = 0;
 	private static var _index:Int = 0;
