9767b1ff961ebda6065927fb736ebb1035c4d8a1
Ben Morris <ben@bendmorris.com>
2016-10-04 21:34:59 -0700

    Add blend mode support to TileShaderRenderer.

--- com/haxepunk/graphics/atlas/Renderer.hx
+++ com/haxepunk/graphics/atlas/Renderer.hx
@@ -1,7 +1,7 @@
 package com.haxepunk.graphics.atlas;
 
 
-#if tile_shader
+#if (openfl > "4.0.0")
 typedef Renderer = com.haxepunk.graphics.atlas.renderer.TileShaderRenderer;
 #else
 typedef Renderer = com.haxepunk.graphics.atlas.renderer.DrawTilesRenderer;
