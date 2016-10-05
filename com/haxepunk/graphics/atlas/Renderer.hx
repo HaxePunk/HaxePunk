89757526dcc4969aea0dca2e574fc995dd6f071f
Ben Morris <ben@bendmorris.com>
2016-10-04 22:22:18 -0700

    Disable hardware rendering on OpenFL4/Flash, use default Preloader on OpenFL4 for now.

--- com/haxepunk/graphics/atlas/Renderer.hx
+++ com/haxepunk/graphics/atlas/Renderer.hx
@@ -1,8 +1,26 @@
 package com.haxepunk.graphics.atlas;
 
 
+@:dox(hide)
+class HardwareNotSupportedRenderer
+{
+	public function new(data:AtlasData) {}
+
+	public function drawTiles(graphics, tileData, smooth, flags, count):Void
+	{
+		throw "hardware rendering not supported on this platform";
+	}
+}
+
+
+
 #if (openfl > "4.0.0")
+#if flash
+// shouldn't be used
+typedef Renderer = HardwareNotSupportedRenderer;
+#else
 typedef Renderer = com.haxepunk.graphics.atlas.renderer.TileShaderRenderer;
+#end
 #else
 typedef Renderer = com.haxepunk.graphics.atlas.renderer.DrawTilesRenderer;
 #end
