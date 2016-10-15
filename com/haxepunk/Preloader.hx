395621bc305775dc87654e94fd1b9cfe33e5eb83
Ben Morris <ben@bendmorris.com>
2016-10-14 22:40:13 -0700

    Update Preloader to use display list.

--- com/haxepunk/Preloader.hx
+++ com/haxepunk/Preloader.hx
@@ -1,37 +1,26 @@
 package com.haxepunk;
 
+import flash.display.Bitmap;
 import flash.display.BitmapData;
 import flash.display.Sprite;
 import flash.events.Event;
 import flash.geom.Point;
 import flash.geom.Rectangle;
 
-#if (openfl >= "4.0.0")
-typedef Preloader = NMEPreloader;
-#else
-import openfl.display.Tilesheet;
-
 @:bitmap("assets/graphics/preloader/haxepunk.png")
 class HaxePunkLogo extends BitmapData {}
 
 class Preloader extends NMEPreloader
 {
+	var largeCog:Sprite;
+	var smallCog:Sprite;
+	var factory:Sprite;
+
 	public function new()
 	{
-		tileData = [
-			  0,   0, 0, 1,  0, // powered by
-			 80, 190, 3, 1, 20, // large cog
-			190, 185, 4, 1, 40, // small cog
-			187, 260, 2, 1,  0, // factory
-			  0, 260, 1, 1,  0, // haxepunk
-		];
+		super();
 
-		tiles = new Tilesheet(new HaxePunkLogo(0, 0));
-		tiles.addTileRect(new Rectangle(0, 0, 274, 58)); // powered by
-		tiles.addTileRect(new Rectangle(0, 65, 274, 80)); // haxepunk
-		tiles.addTileRect(new Rectangle(0, 165, 114, 190), new Point(57, 190)); // factory
-		tiles.addTileRect(new Rectangle(115, 164, 134, 136), new Point(67, 68)); // large cog
-		tiles.addTileRect(new Rectangle(123, 305, 56, 56), new Point(28, 28)); // small cog
+		var bmd = new HaxePunkLogo(0, 0);
 
 		scaleIncrement = 0.002;
 
@@ -42,11 +31,51 @@ class Preloader extends NMEPreloader
 		var color = 0xFFCB6325;
 		var padding = 5;
 
-		super();
-
 		outline.x = (getWidth() - width) / 2;
 		outline.y = (getHeight() - height) / 2;
 		outline.graphics.clear();
+
+		// powered by
+		var img = new Bitmap(crop(bmd, new Rectangle(0, 0, 274, 58)));
+		addChild(img);
+		img.x = outline.x;
+		img.y = outline.y;
+
+		// haxepunk
+		var img = new Bitmap(crop(bmd, new Rectangle(0, 65, 274, 80)));
+		addChild(img);
+		img.x = outline.x;
+		img.y = outline.y + 260;
+
+		// factory
+		factory = new Sprite();
+		factory.x = outline.x + 187;
+		factory.y = outline.y + 260;
+		var img = new Bitmap(crop(bmd, new Rectangle(0, 165, 114, 190)));
+		img.x = -57;
+		img.y = -190;
+		factory.addChild(img);
+		addChild(factory);
+
+		// large cog
+		largeCog = new Sprite();
+		largeCog.x = outline.x + 80;
+		largeCog.y = outline.y + 190;
+		var img = new Bitmap(crop(bmd, new Rectangle(115, 164, 134, 136)));
+		img.x = -67;
+		img.y = -68;
+		largeCog.addChild(img);
+		addChild(largeCog);
+
+		// small cog
+		smallCog = new Sprite();
+		smallCog.x = outline.x + 190;
+		smallCog.y = outline.y + 185;
+		var img = new Bitmap(crop(bmd, new Rectangle(123, 305, 56, 56)));
+		img.x = -28;
+		img.y = -28;
+		smallCog.addChild(img);
+		addChild(smallCog);
 
 		var complete = new Sprite ();
 		complete.x = outline.x + width / 2;
@@ -69,19 +98,22 @@ class Preloader extends NMEPreloader
 
 	public function onEnterFrame(e:Event)
 	{
-		tileData[9] += 1; // large cog
-		tileData[14] += 1; // small cog
-		tileData[18] += scaleIncrement; // factory scale
-		if (tileData[18] > 1.02 || tileData[18] < 1)
+		largeCog.rotation += 1;
+		smallCog.rotation -= 1;
+		factory.scaleX += scaleIncrement;
+		factory.scaleY += scaleIncrement;
+		if (factory.scaleX > 1.02 || factory.scaleX < 1)
 			scaleIncrement = -scaleIncrement;
 
 		outline.graphics.clear();
-		tiles.drawTiles(outline.graphics, tileData, true, Tilesheet.TILE_ROTATION | Tilesheet.TILE_SCALE);
+	}
+
+	function crop(bmd:BitmapData, rect:Rectangle):BitmapData
+	{
+		var cropped = new BitmapData(Std.int(rect.width), Std.int(rect.height));
+		cropped.copyPixels(bmd, rect, new Point());
+		return cropped;
 	}
 
 	private var scaleIncrement:Float;
-
-	private var tiles:Tilesheet;
-	private var tileData:Array<Float>;
 }
-#end
