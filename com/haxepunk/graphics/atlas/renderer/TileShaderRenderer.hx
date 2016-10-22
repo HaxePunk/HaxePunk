535a843830070da2d442f655bf47791063d9b53c
Ben Morris <ben@bendmorris.com>
2016-10-21 23:01:53 -0700

    Don't use OpenFL ShaderManager for TileShaderRenderer.

--- com/haxepunk/graphics/atlas/renderer/TileShaderRenderer.hx
+++ com/haxepunk/graphics/atlas/renderer/TileShaderRenderer.hx
@@ -9,45 +9,89 @@ import openfl.display.DisplayObject;
 import openfl.display.Graphics;
 import openfl.display.Shader;
 import openfl.geom.Matrix;
+import openfl.gl.GL;
 import openfl.gl.GLBuffer;
+import openfl.gl.GLProgram;
 import openfl._internal.renderer.opengl.GLRenderer;
 import com.haxepunk.HXP;
 
 @:dox(hide)
-private class TileShader extends Shader
+private class TileShader
 {
+	public var glProgram:GLProgram;
+
+	public static inline var VERTEX_SHADER = "
+attribute vec4 aPosition;
+attribute vec2 aTexCoord;
+attribute vec4 aColor;
+varying vec2 vTexCoord;
+varying vec4 vColor;
+uniform mat4 uMatrix;
+
+void main(void) {
+	vTexCoord = aTexCoord;
+	vColor = aColor;
+	gl_Position = uMatrix * aPosition;
+}";
+
+	public static inline var FRAGMENT_SHADER = "
+#version 120
+
+#ifdef GL_ES
+    precision mediump float;
+#endif
+
+varying vec2 vTexCoord;
+varying vec4 vColor;
+uniform sampler2D uImage0;
+
+void main(void) {
+	vec4 color = texture2D (uImage0, vTexCoord);
+	if (color.a == 0.0) {
+		gl_FragColor = vec4 (0.0, 0.0, 0.0, 0.0);
+	} else {
+		gl_FragColor = vec4 (color.rgb / color.a, color.a)*vColor;
+	}
+}";
+
 	public function new()
 	{
-		glVertexSource =
-			"attribute vec4 aPosition;
-			attribute vec2 aTexCoord;
-			attribute vec4 aColor;
-			varying vec2 vTexCoord;
-			varying vec4 vColor;
-			uniform mat4 uMatrix;
+		var vertexShader = GL.createShader(GL.VERTEX_SHADER);
+		GL.shaderSource(vertexShader, VERTEX_SHADER);
+		GL.compileShader(vertexShader);
+		if (GL.getShaderParameter(vertexShader, GL.COMPILE_STATUS) == 0)
+			throw "Error compiling vertex shader";
 
-			void main(void) {
-				vTexCoord = aTexCoord;
-				vColor = aColor;
-				gl_Position = uMatrix * aPosition;
-			}";
+		var fragmentShader = GL.createShader(GL.FRAGMENT_SHADER);
+		GL.shaderSource(fragmentShader, FRAGMENT_SHADER);
+		GL.compileShader(fragmentShader);
+		if (GL.getShaderParameter(fragmentShader, GL.COMPILE_STATUS) == 0)
+			throw "Error compiling fragment shader";
 
-		glFragmentSource =
-			"varying vec2 vTexCoord;
-			varying vec4 vColor;
-			uniform sampler2D uImage0;
-			uniform float uAlpha;
+		glProgram = GL.createProgram();
+		GL.attachShader(glProgram, vertexShader);
+		GL.attachShader(glProgram, fragmentShader);
+		GL.linkProgram(glProgram);
+		if (GL.getProgramParameter(glProgram, GL.LINK_STATUS) == 0)
+			throw "Unable to initialize the shader program.";
+	}
 
-			void main(void) {
-				vec4 color = texture2D (uImage0, vTexCoord);
-				if (color.a == 0.0) {
-					gl_FragColor = vec4 (0.0, 0.0, 0.0, 0.0);
-				} else {
-					gl_FragColor = vec4 (color.rgb / color.a, color.a * uAlpha)*vColor;
-				}
-			}";
+	public function bind()
+	{
+		GL.useProgram(glProgram);
+		GL.enableVertexAttribArray(GL.getAttribLocation(glProgram, "aPosition"));
+		GL.enableVertexAttribArray(GL.getAttribLocation(glProgram, "aTexCoord"));
+		GL.enableVertexAttribArray(GL.getAttribLocation(glProgram, "aColor"));
+	}
 
-		super();
+	public inline function attributeIndex(name:String)
+	{
+		return GL.getAttribLocation(glProgram, name);
+	}
+
+	public inline function uniformIndex(name:String)
+	{
+		return GL.getUniformLocation(glProgram, name);
 	}
 }
 
@@ -207,9 +251,9 @@ class TileShaderRenderer
 				buffer[bufferPos++] = alpha;
 			}
 
-			renderSession.shaderManager.setShader(shader);
-			gl.uniform1f(shader.data.uAlpha.index, displayObject.__worldAlpha);
-			gl.uniformMatrix4fv(shader.data.uMatrix.index, false, new lime.utils.Float32Array(renderer.getMatrix(displayObject.__renderTransform)));
+			shader.bind();
+			gl.uniform1f(shader.uniformIndex("uAlpha"), displayObject.__worldAlpha);
+			gl.uniformMatrix4fv(shader.uniformIndex("uMatrix"), false, new lime.utils.Float32Array(renderer.getMatrix(displayObject.__renderTransform)));
 
 			renderSession.blendModeManager.setBlendMode(cast blend);
 
@@ -237,14 +281,16 @@ class TileShaderRenderer
 			gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, glIndexes);
 			gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexes, gl.DYNAMIC_DRAW);
 
-			gl.vertexAttribPointer(shader.data.aPosition.index, 2, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 0);
-			gl.vertexAttribPointer(shader.data.aTexCoord.index, 2, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
-			gl.vertexAttribPointer(shader.data.aColor.index, 4, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 4 * Float32Array.BYTES_PER_ELEMENT);
+			gl.vertexAttribPointer(shader.attributeIndex("aPosition"), 2, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 0);
+			gl.vertexAttribPointer(shader.attributeIndex("aTexCoord"), 2, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+			gl.vertexAttribPointer(shader.attributeIndex("aColor"), 4, gl.FLOAT, false, 8 * Float32Array.BYTES_PER_ELEMENT, 4 * Float32Array.BYTES_PER_ELEMENT);
 
 			gl.scissor(HXP.screen.x, HXP.screen.y, HXP.screen.width, HXP.screen.height);
 			gl.enable(gl.SCISSOR_TEST);
 			gl.drawElements(gl.TRIANGLES, items * INDEX_CHUNK, gl.UNSIGNED_INT, 0);
 			gl.disable(gl.SCISSOR_TEST);
+
+			renderSession.shaderManager.setShader(null);
 		}
 	}
 }
