dcc5e9c52293eb34ff83205cd970b9cb996a5318
Ben Morris <ben@bendmorris.com>
2017-02-07 23:55:10 -0800

    Random cleanup.

--- haxepunk/utils/Random.hx
+++ haxepunk/utils/Random.hx
@@ -10,11 +10,11 @@ class Random
 	/**
 	 * The random seed used by HXP's random functions.
 	 */
-	public static var randomSeed(default, set):Int = 0;
-	private static inline function set_randomSeed(value:Int):Int
+	public static var randomSeed(get, set):Int;
+	static inline function get_randomSeed() return _seed;
+	static inline function set_randomSeed(value:Int):Int
 	{
 		_seed = Std.int(MathUtil.clamp(value, 1.0, MathUtil.INT_MAX_VALUE - 1));
-		randomSeed = _seed;
 		return _seed;
 	}
 
@@ -30,7 +30,7 @@ class Random
 	 * A pseudo-random Float produced using HXP's random seed, where 0 <= Float < 1.
 	 */
 	public static var random(get, null):Float;
-	private static inline function get_random():Float
+	static inline function get_random():Float
 	{
 		_seed = Std.int((_seed * 16807.0) % MathUtil.INT_MAX_VALUE);
 		return _seed / MathUtil.INT_MAX_VALUE;
@@ -47,7 +47,6 @@ class Random
 		return Std.int((_seed / MathUtil.INT_MAX_VALUE) * amount);
 	}
 
-
 	// Pseudo-random number generation (the seed is set in Engine's contructor).
-	private static var _seed:Int = 0;
+	static var _seed:Int = 0;
 }
