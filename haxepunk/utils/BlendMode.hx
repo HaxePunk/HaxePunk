a11ad1295ee20ef8f30c29f0543b77777a4227a4
Ben Morris <ben@bendmorris.com>
2017-06-21 23:27:25 -0700

    Fix for BlendMode implicit casting in NME.

--- haxepunk/utils/BlendMode.hx
+++ haxepunk/utils/BlendMode.hx
@@ -8,7 +8,19 @@ abstract BlendMode(Int) from Int to Int {
 	public var Screen = 12;
 	public var Subtract = 14;
 
-#if openfl
+#if nme
+	@:from public static function fromNmeBlendMode(blend:nme.display.BlendMode):BlendMode
+	{
+		return switch (blend)
+		{
+			case ADD: Add;
+			case MULTIPLY: Multiply;
+			case SCREEN: Screen;
+			case SUBTRACT: Subtract;
+			default: Alpha;
+		}
+	}
+#elseif openfl
 	@:from public static function fromFlashBlendMode(blend:flash.display.BlendMode):BlendMode return cast blend;
 #end
 }
