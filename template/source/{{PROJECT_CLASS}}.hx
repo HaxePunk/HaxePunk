599405dc2bf4a6fd10fbc5a3ff7fbdf42afb3595
Matt Tuttle <heardtheword@gmail.com>
2017-07-24 17:07:19 -0500

    Fixing visual studio completion for template [ci skip]

--- template/source/{{PROJECT_CLASS}}.hx
+++ template/source/{{PROJECT_CLASS}}.hx
@@ -1,14 +1,10 @@
 import haxepunk.Engine;
 import haxepunk.HXP;
-import haxepunk.debug.Console;
 
 class {{PROJECT_CLASS}} extends Engine
 {
 	override public function init()
 	{
-#if (debug || console)
-		Console.enable();
-#end
 		HXP.scene = new MainScene();
 	}
 }
