d15ee305adbdec1cbe5729f0d28f10421ac18056
matrefeytontias <mattias@refeyton.fr>
2018-04-06 00:29:02 +0200

    Corrected Xbox bindings for Windows

--- haxepunk/input/gamepads/XboxGamepad.hx
+++ haxepunk/input/gamepads/XboxGamepad.hx
@@ -73,23 +73,23 @@ class XboxGamepad
 	/**
 	 * Button IDs
 	 */
-	public static inline var A_BUTTON:Int = 10;
-	public static inline var B_BUTTON:Int = 11;
-	public static inline var X_BUTTON:Int = 12;
-	public static inline var Y_BUTTON:Int = 13;
-	public static inline var LB_BUTTON:Int = 8;
-	public static inline var RB_BUTTON:Int = 9;
-	public static inline var BACK_BUTTON:Int = 5;
-	public static inline var START_BUTTON:Int = 4;
-	public static inline var LEFT_ANALOGUE_BUTTON:Int = 6;
-	public static inline var RIGHT_ANALOGUE_BUTTON:Int = 7;
+	public static inline var A_BUTTON:Int = 0;
+	public static inline var B_BUTTON:Int = 1;
+	public static inline var X_BUTTON:Int = 2;
+	public static inline var Y_BUTTON:Int = 3;
+	public static inline var BACK_BUTTON:Int = 4;
+	public static inline var START_BUTTON:Int = 6;
+	public static inline var LEFT_ANALOGUE_BUTTON:Int = 7;
+	public static inline var RIGHT_ANALOGUE_BUTTON:Int = 8;
+	public static inline var LB_BUTTON:Int = 9;
+	public static inline var RB_BUTTON:Int = 10;
 
-	public static inline var XBOX_BUTTON:Int = 14;
+	public static inline var XBOX_BUTTON:Int = 5;
 
-	public static inline var DPAD_UP:Int = 0;
-	public static inline var DPAD_DOWN:Int = 1;
-	public static inline var DPAD_LEFT:Int = 2;
-	public static inline var DPAD_RIGHT:Int = 3;
+	public static inline var DPAD_UP:Int = 11;
+	public static inline var DPAD_DOWN:Int = 12;
+	public static inline var DPAD_LEFT:Int = 13;
+	public static inline var DPAD_RIGHT:Int = 14;
 
 	/**
 	 * Axis array indicies
