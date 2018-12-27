e37172758b55979734c3633012f7053f70cc456b
Ghost <29788070+Ghouly-The-Ghost@users.noreply.github.com>
2018-12-26 20:47:44 -0700

    Added second needed case
    Originally I thought Entity was the only class cared to sync with, but I was wrong. This update corrects that and refactors some of the original code to be reusable between tests.

--- tests/src/haxepunk/CameraTest.hx
+++ tests/src/haxepunk/CameraTest.hx
@@ -3,35 +3,69 @@ package haxepunk;
 import haxepunk.Camera;
 import haxepunk.Entity;
 import haxepunk.HXP;
+import haxepunk.math.Vector2;
 
 class CameraTest
 {
-	@Test
-	public function testCameraAnchor()
+	var widthSave:Int;
+	var heightSave:Int;
+
+	@Before
+	public function setup():Void
 	{
-		var e = new Entity();
-		var camera = new Camera();
-		
-		// Set null information to 0
-		var widthSave = HXP.width,
-			heightSave = HXP.height;
+		widthSave = HXP.width;
+		heightSave = HXP.height;
 		HXP.width = 0;
 		HXP.height = 0;
+	}
+
+	@After
+	public function tearDown():Void
+	{
+		HXP.width = heightSave;
+		HXP.height = heightSave;
+	}
+
+	// @Test
+	public function testCameraAnchorEntity()
+	{
+		var e = new Entity(0,2);
+		var camera = new Camera();
 
 		camera.anchor(e);
-		Assert.areEqual(e.x, camera.x);
-		Assert.areEqual(e.y, camera.y);
-		e.x = 100;
-		e.y = 500;
-		Assert.areNotEqual( e.x, camera.x ); //shouldn't change
-		Assert.areNotEqual( e.y, camera.y ); //shouldn't change
 		camera.update();
 		Assert.areEqual( e.x, camera.x );
 		Assert.areEqual( e.y, camera.y );
 		
-		// Reset original values
-		HXP.width = HXP.width;
-		HXP.height = heightSave;
+		e.x = 100;
+		e.y = 500;
+		Assert.areNotEqual( e.x, camera.x ); //shouldn't change
+		Assert.areNotEqual( e.y, camera.y ); //shouldn't change
+		
+		camera.update();
+		Assert.areEqual( e.x, camera.x );
+		Assert.areEqual( e.y, camera.y );
+	}
+
+	@Test
+	public function testCameraAnchorOther()
+	{
+		var v = new Vector2(0,2);
+		var camera = new Camera();
+
+		camera.anchor(v);
+		camera.update();
+		Assert.areEqual( v.x, camera.x );
+		Assert.areEqual( v.y, camera.y );
+		
+		v.x = 100;
+		v.y = 500;
+		Assert.areNotEqual( v.x, camera.x ); //shouldn't change
+		Assert.areNotEqual( v.y, camera.y ); //shouldn't change
+		
+		camera.update();
+		Assert.areEqual( v.x, camera.x );
+		Assert.areEqual( v.y, camera.y );
 	}
 
 }
