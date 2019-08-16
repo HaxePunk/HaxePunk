89b0da2a42d644041d83cadc8b1baedf6096c2fd
Xzod <29788070+XANOZOID@users.noreply.github.com>
2019-08-16 05:07:01 -0700

    Signals updated to reflect Haxe 4 syntax removal

--- haxepunk/Signal.hx
+++ haxepunk/Signal.hx
@@ -66,22 +66,43 @@ class Signal2<A, B> extends Signal<A->B->Void>
 /**
  * A collection of named signals, which can be accessed as attributes.
  */
-class Signals implements Dynamic<Signal0>
-{
-	var signals:Map<String, Signal0> = new Map();
+abstract Signals(Map<String, Signal0>) from Map<String, Signal0> {
 
-	public function new() {}
-
-	public inline function exists(field:String) return signals.exists(field);
-
-	public inline function invoke(field:String)
+	public function new()
 	{
-		if (exists(field)) signals[field].invoke();
+		this = new Map<String, Signal0>();
 	}
 
-	public function resolve(field:String):Signal0
+	@:op([]) inline public function arrayRead(field:String)
 	{
-		if (!exists(field)) signals[field] = new Signal0();
-		return signals[field];
+		return this[field];
+	}
+
+	@:op([]) inline public function arrayWrite(field:String, signal:Signal0)
+	{
+		return this[field] = signal;
+	}
+
+	@:op(a.b) inline public function fieldRead(field:String)
+	{
+		return resolve( field);
+	}
+
+	@:op(a.b) inline public function fieldWrite(field:String, signal:Signal0)
+	{
+		return this[field] = signal;
+	}
+
+	public inline function exists(field:String) return this.exists(field);
+	
+	public inline function invoke(field:String)
+	{
+		if (exists(field)) this[field].invoke();
+	}
+
+	public function resolve(field:String)
+	{
+		if (!exists(field)) this[field] = new Signal0();
+		return this[field];
 	}
 }
